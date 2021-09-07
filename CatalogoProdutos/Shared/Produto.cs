fb813e121ea36ae930b7891c8bca5262adb0ba61
Antônio Vinícius <antoniovrdacosta@gmail.com>
2021-09-07 17:54:50 -0300

    Funcionando em memória

--- CatalogoProdutos/Shared/Produto.cs
+++ CatalogoProdutos/Shared/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace CatalogoProdutos.Shared
     public class Produto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Preço é obrigatório")]
         public double Preco { get; set; }
+        [Required(ErrorMessage = "Categoria é obrigatório")]
         public int CategoriaId { get; set; }
         public virtual Categoria Categoria { get; set; }
 
