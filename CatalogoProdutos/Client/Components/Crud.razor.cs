09acf2889825c62b7b874f00799c93f12e204756
Antônio Vinícius <antoniovrdacosta@gmail.com>
2021-09-11 14:00:15 -0300

    Crud categoria

---
+++ CatalogoProdutos/Client/Components/Crud.razor.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace CatalogoProdutos.Client.Components
+{
+    public abstract partial class Crud<T, TDialog> where TDialog : ComponentBase
+    {
+        [Parameter]
+        public string ApiResource { get; set; }
+        [Parameter]
+        public RenderFragment ItemIndividual { get; set; }
+        [Parameter]
+        public RenderFragment ItemAdicionar { get; set; }
+        [Parameter]
+        public T Model { get; set; }
+
+        // Carrega
+        async Task LoadItems()
+        {
+            try
+            {
+                items = await http.GetFromJsonAsync<List<T>>($"api/{ApiResource}");
+            }
+            catch (Exception)
+            {
+                Snackbar.Add("Ocorreu um erro ao tentar recuperar os items", Severity.Error);
+            }
+        }
+
+        async Task OnValidSubmit()
+        {
+            try
+            {
+                await http.PostAsJsonAsync<T>($"api/{ApiResource}", Model);
+                await LoadItems();
+                Snackbar.Add("Item adicionado", Severity.Success);
+            }
+            catch (Exception)
+            {
+                Snackbar.Add("Ocorreu um erro ao tentar adicionar o item", Severity.Error);
+            }
+        }
+
+        // Lista
+        List<T> items;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadItems();
+        }
+
+        // Delete
+        async Task Delete(int id)
+        {
+            try
+            {
+                await http.DeleteAsync($"api/{ApiResource}/{id}");
+                await LoadItems();
+            }
+            catch (Exception)
+            {
+                Snackbar.Add("Ocorreu um erro ao tentar deletar uma item", Severity.Error);
+            }
+        }
+
+        // Editar
+        DialogOptions maxWidth = new DialogOptions() { MaxWidth = MaxWidth.Medium, FullWidth = true };
+        async Task Editar(T item)
+        {
+            try
+            {
+                var parameters = new DialogParameters { ["item"] = item };
+
+                var dialog = DialogService.Show<TDialog>("Editar item", parameters, maxWidth);
+                var result = await dialog.Result;
+
+                if (!result.Cancelled)
+                {
+                    await LoadItems();
+                }
+            }
+            catch (Exception)
+            {
+                Snackbar.Add("Ocorreu um erro ao tentar editar uma item", Severity.Error);
+            }
+        }
+    }
+}
--- CatalogoProdutos/Client/Program.cs
+++ CatalogoProdutos/Client/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
+using MudBlazor;
 
 namespace CatalogoProdutos.Client
 {
@@ -16,7 +17,15 @@ namespace CatalogoProdutos.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddMudServices();
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.VisibleStateDuration = 3000;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Outlined;
+            });
             builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
